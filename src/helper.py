990c8d5cae6a5bf60d8c0805c941b2a07359be22
luigiminardim <luigiminardim@gmail.com>
2024-05-11 12:20:40 -0300

    refactor: :recycle: use Jimmy Song tests in helper

--- src/helper.py
+++ src/helper.py
@@ -3,6 +3,16 @@ import hashlib
 BASE58_ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
 
+def hash160(s):
+    """sha256 followed by ripemd160"""
+    return hashlib.new("ripemd160", hashlib.sha256(s).digest()).digest()
+
+
+def hash256(s):
+    """two rounds of sha256"""
+    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
+
+
 def encode_base58(s):
     count = 0
     for c in s:
@@ -19,18 +29,22 @@ def encode_base58(s):
     return prefix + result
 
 
-def hash256(s):
-    """two rounds of sha256"""
-    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
-
-
-def hash160(s):
-    """sha256 followed by ripemd160"""
-    return hashlib.new("ripemd160", hashlib.sha256(s).digest()).digest()
-
-
 def encode_base58_checksum(b):
     return encode_base58(b + hash256(b)[:4])
+
+
+def decode_base58(s):
+    num = 0
+    for c in s:
+        num *= 58
+        num += BASE58_ALPHABET.index(c)
+    combined = num.to_bytes(25, byteorder="big")
+    checksum = combined[-4:]
+    if hash256(combined[:-4])[:4] != checksum:
+        raise ValueError(
+            "bad address: {} {}".format(checksum, hash256(combined[:-4])[:4])
+        )
+    return combined[1:-4]
 
 
 def little_endian_to_int(b):
