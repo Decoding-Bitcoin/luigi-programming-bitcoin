be16caea90c81284f6dbf86cf62211e2f4823380
Luigi Minardi <luigiminardim@gmail.com>
2024-05-13 19:34:31 -0300

    refactor: :recycle: use Jimmy Song tests for chapter 6

--- src/script.py
+++ src/script.py
@@ -14,6 +14,11 @@ from op import (
 )
 
 
+def p2pkh_script(h160):
+    """Takes a hash160 and returns the p2pkh ScriptPubKey"""
+    return Script([0x76, 0xA9, h160, 0x88, 0xAC])
+
+
 class Script:
 
     def __init__(self, cmds=None):
--- src/tx_test.py
+++ src/tx_test.py
@@ -1,8 +1,14 @@
 from tx import Tx, TxFetcher
 import io
+from ecc import PrivateKey
 
 
 class TestTx:
+
+    @classmethod
+    def setUpClass(cls):
+        # fill with cache so we don't have to be online to run these tests
+        TxFetcher.load_cache(cls.cache_file)
 
     def test_parse_version(self):
         raw_tx = bytes.fromhex(
@@ -67,3 +73,41 @@ class TestTx:
         stream = io.BytesIO(raw_tx)
         tx = Tx.parse(stream)
         assert tx.fee() == 140500
+
+    def test_sig_hash(self):
+        tx = TxFetcher.fetch(
+            "452c629d67e41baec3ac6f04fe744b4b9617f8f859c63b3002f8684e7a4fee03"
+        )
+        want = int(
+            "27e0c5994dec7824e56dec6b2fcb342eb7cdb0d0957c2fce9882f715e85d81a6", 16
+        )
+        assert tx.sig_hash(0) == want
+
+    def test_verify_p2pkh(self):
+        tx = TxFetcher.fetch(
+            "452c629d67e41baec3ac6f04fe744b4b9617f8f859c63b3002f8684e7a4fee03"
+        )
+        assert tx.verify()
+        tx = TxFetcher.fetch(
+            "5418099cc755cb9dd3ebc6cf1a7888ad53a1a3beb5a025bce89eb1bf7f1650a2",
+            testnet=True,
+        )
+        assert tx.verify()
+
+    def test_verify_p2sh(self):
+        tx = TxFetcher.fetch(
+            "46df1a9484d0a81d03ce0ee543ab6e1a23ed06175c104a178268fad381216c2b"
+        )
+        assert tx.verify()
+
+    def test_sign_input(self):
+        private_key = PrivateKey(secret=8675309)
+        stream = io.BytesIO(
+            bytes.fromhex(
+                "010000000199a24308080ab26e6fb65c4eccfadf76749bb5bfa8cb08f291320b3c21e56f0d0d00000000ffffffff02408af701000000001976a914d52ad7ca9b3d096a38e752c2018e6fbc40cdf26f88ac80969800000000001976a914507b27411ccf7f16f10297de6cef3f291623eddf88ac00000000"
+            )
+        )
+        tx_obj = Tx.parse(stream, testnet=True)
+        assert tx_obj.sign_input(0, private_key)
+        want = "010000000199a24308080ab26e6fb65c4eccfadf76749bb5bfa8cb08f291320b3c21e56f0d0d0000006b4830450221008ed46aa2cf12d6d81065bfabe903670165b538f65ee9a3385e6327d80c66d3b502203124f804410527497329ec4715e18558082d489b218677bd029e7fa306a72236012103935581e52c354cd2f484fe8ed83af7a3097005b2f9c60bff71d35bd795f54b67ffffffff02408af701000000001976a914d52ad7ca9b3d096a38e752c2018e6fbc40cdf26f88ac80969800000000001976a914507b27411ccf7f16f10297de6cef3f291623eddf88ac00000000"
+        assert tx_obj.serialize().hex() == want
